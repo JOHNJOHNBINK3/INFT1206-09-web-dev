adbdb1dd18d385aae5101bf3399e351ffb5b4cfb
JOHNJOHNBINK3 <156018671+JOHNJOHNBINK3@users.noreply.github.com>
2024-03-15 14:36:40 -0400

    added raw text strings

--- assignment4/Part1/main.js
+++ assignment4/Part1/main.js
@@ -6,3 +6,15 @@ function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
+
+
+// Step 2
+
+storyText = `It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.`
+
+insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas']
+insertY = ['the soup kitchen', 'Disneyland', 'the White House']
+insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away']
+
+//Step 3
+
